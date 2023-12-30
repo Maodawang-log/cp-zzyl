0dfd2f5c90de83eefc634eae8f67dbfb8f43c995
yuhongbo <yuhongbo@itcast.cn>
2023-12-30 16:15:29 +0800

    dev修改了引导类的第11行代码

--- zzyl-web/src/main/java/com/zzyl/ZzylApplication.java
+++ zzyl-web/src/main/java/com/zzyl/ZzylApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ZzylApplication {
 
 	public static void main(String[] args) {
+		System.out.println("1111");
 		SpringApplication.run(ZzylApplication.class, args);
 	}
 }
