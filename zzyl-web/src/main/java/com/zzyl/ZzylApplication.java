2eb4033e71a3957ce83d839542cdb05c5af647b6
yuhongbo <yuhongbo@itcast.cn>
2023-12-30 16:19:15 +0800

    测试冲突

--- zzyl-web/src/main/java/com/zzyl/ZzylApplication.java
+++ zzyl-web/src/main/java/com/zzyl/ZzylApplication.java
@@ -9,6 +9,7 @@ public class ZzylApplication {
 
 	public static void main(String[] args) {
 		System.out.println("1111");
+		System.out.println("6666");
 		SpringApplication.run(ZzylApplication.class, args);
 	}
 }
